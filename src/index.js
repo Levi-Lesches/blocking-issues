d6abc8dd4c383da00a4c84d94f74641a10151182
Levi Lesches <levilesches@gmail.com>
2021-05-11 03:45:51 -0400

    Some async shenanigans

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const core = require('@actions/core');
 const model = require('./model.js');
 
-try {
-	// const octokit = github.getOctokit(github.token);
+async function main() {
+	try {
+		// const octokit = github.getOctokit(github.token);
 
-	console.log("Hello, World!");
-	console.log(model.getCurrentIssue());
-} catch (error) {
-	core.setFailed(error.message);
+		console.log("Hello, World!");
+		console.log(await model.getCurrentIssue());
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
+
+main()
