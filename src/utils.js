02a8366953650c0d1a4f0934057bbc87129503f5
Levi Lesches <levilesches@gmail.com>
2022-07-18 21:16:31 -0400

    Correctly parse empty bodies

--- src/utils.js
+++ src/utils.js
@@ -12,6 +12,7 @@ const blockedLabel = {
 
 function getBlockingIssues(body) {
 	issues = [];
+	if (body === null) return issues;
 	for (match of body.matchAll(regex)) {
 		for (issue of match [1].split(", ")) {
 			issueNumber = parseInt(issue.substring(1));
