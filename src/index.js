4e077187f78b865d37a81328444a9b27929e5398
Levi Lesches <levilesches@gmail.com>
2022-07-19 21:41:00 -0400

    Fixed output message

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ async function main() {
 		core.debug(issue);
 
 		if (issue.state === "open") {
-			const isReady = await model.update(issue);
+			const isBlocked = await model.update(issue);
 
-			if (isReady === undefined) core.info("No blocking issues found."); 
-			else if (isReady === false) core.setFailed("Issue/PR is blocked.");
-			else if (isReady === true) core.info("Issue/PR is not blocked.");
+			if (isBlocked === undefined) core.info("No blocking issues found."); 
+			else if (isBlocked === true) core.setFailed("Issue/PR is blocked.");
+			else if (isBlocked === false) core.info("Issue/PR is not blocked.");
 		} else {
 			core.info("Issue is closed. Unblocking other issues...");
 			await model.unblockPRs(issue.number);
